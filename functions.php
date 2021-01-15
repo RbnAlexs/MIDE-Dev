8c38feb0274d6804ab31323ba9c2f7858c9ab53d
Ruben Sanchez <rubensanchezcolin@gmail.com>
2021-01-15 00:29:31 -0600

    Fabrica de billetes v.1.4

--- functions.php
+++ functions.php
@@ -12,9 +12,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 function fabrica_scripts() {
 	if ( is_page('fabrica-de-billetes') ) {
 
-/*  		wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.min.js',  array('jquery'), false, true );
- */
-		wp_enqueue_script('bootstrap_js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.5.1', true);
+ 		wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery.min.js',  array('jquery'), false, true );
+
+		wp_enqueue_script('bootstrap_js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.4.1', true);
  
 		wp_enqueue_script('cropper_js', get_stylesheet_directory_uri() . '/js/cropper.js', false, true);
  
